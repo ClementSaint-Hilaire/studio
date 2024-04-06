9aaa63866da69c377895e590832199caa1c5b8e3
cshd <clement.sainthilaire@sts-sio-caen.info>
2024-04-06 22:51:06 +0200

    bases

---
+++ php/navbar.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="../img/logo.png" />
+    <link rel="stylesheet" href="../css/navbar.css">
+    <title>csh</title>
+</head>
+<body>
+
+    <section class="navbar_container">
+        <div class="content_left">
+            <a href="#home">HOME</a>
+            <a href="#creations">CREATIONS</a>
+            <a href="#about">ABOUT</a>
+            <a href="/studio/index.php">STUDIO</a>
+            <div class="new">
+                <h1>NEW</h1>
+                <a href="/cards/php/accueil.php" target="_blank">CARDS</a>
+            </div>
+        </div>
+        <div class="content_right">
+            <a href="#contact">CONTACT</a>
+            <button class="menu" id="topButton">
+                <svg width="28" height="14" viewBox="0 0 28 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect x="0.238098" y="0.9375" width="27" height="3" rx="1.5" fill="#FAFAFA"/>
+                    <rect x="12.2381" y="10.0625" width="15" height="3" rx="1.5" fill="#FAFAFA"/>
+                </svg>
+            </button>
+            <button class="close">
+                <svg width="45" height="44" viewBox="0 0 45 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M37.3518 2.12132C38.5234 0.949746 40.4229 0.949748 41.5945 2.12132C42.766 3.29289 42.766 5.19239 41.5945 6.36396L6.3637 41.5947C5.19212 42.7663 3.29263 42.7663 2.12106 41.5947C0.949485 40.4232 0.949486 38.5237 2.12106 37.3521L37.3518 2.12132Z" fill="#FAFAFA"/>
+                    <path d="M7.64885 2.12132C6.47727 0.949746 4.57778 0.949748 3.40621 2.12132C2.23463 3.29289 2.23463 5.19239 3.40621 6.36396L38.637 41.5947C39.8085 42.7663 41.708 42.7663 42.8796 41.5947C44.0512 40.4232 44.0512 38.5237 42.8796 37.3521L7.64885 2.12132Z" fill="#FAFAFA"/>
+                </svg>
+            </button>
+        </div>
+    </section>
+
+</body>
+<script src="../js/menu.js"></script>
+</html>
