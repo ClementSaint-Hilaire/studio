fcbf95621a9ededd397708ddffcfba4b7b133f36
cshd <clement.sainthilaire@sts-sio-caen.info>
2024-04-07 17:45:46 +0200

    scroll en cours

--- php/homepage.php
+++ php/homepage.php
@@ -10,7 +10,7 @@
 <body>
     <?php include_once './navbar.php'; ?>
 
-    <section class="landingpage">
+    <section class="landingpage" id="landingpage">
         <div class="textEtImageContainer">
             <div class="textContainer">
                 <h1>csh Studio</h1>
@@ -18,45 +18,39 @@
             </div>
             <img src="../img/studio display.png" id="studioImage">
         </div>
-        
     </section>
-    <div>
-            <h1>La vidéo de non-responsabilité est uniquement destinée à des fins éducatives. Clause de non-responsabilité relative aux droits d'auteur En vertu de l'article 107 de la loi sur le droit d'auteur de 1976, une « utilisation équitable » est autorisée à des fins telles que la critique, les commentaires, les reportages d'actualité, l'enseignement, l'érudition et la recherche. L'utilisation équitable est une utilisation autorisée par la loi sur le droit d'auteur qui pourrait autrement constituer une violation. Une utilisation à but non lucratif, éducative ou personnelle fait pencher la balance en faveur d'une utilisation équitable.</h1>
-            <h1>La vidéo de non-responsabilité est uniquement destinée à des fins éducatives. Clause de non-responsabilité relative aux droits d'auteur En vertu de l'article 107 de la loi sur le droit d'auteur de 1976, une « utilisation équitable » est autorisée à des fins telles que la critique, les commentaires, les reportages d'actualité, l'enseignement, l'érudition et la recherche. L'utilisation équitable est une utilisation autorisée par la loi sur le droit d'auteur qui pourrait autrement constituer une violation. Une utilisation à but non lucratif, éducative ou personnelle fait pencher la balance en faveur d'une utilisation équitable.</h1>
-            <h1>La vidéo de non-responsabilité est uniquement destinée à des fins éducatives. Clause de non-responsabilité relative aux droits d'auteur En vertu de l'article 107 de la loi sur le droit d'auteur de 1976, une « utilisation équitable » est autorisée à des fins telles que la critique, les commentaires, les reportages d'actualité, l'enseignement, l'érudition et la recherche. L'utilisation équitable est une utilisation autorisée par la loi sur le droit d'auteur qui pourrait autrement constituer une violation. Une utilisation à but non lucratif, éducative ou personnelle fait pencher la balance en faveur d'une utilisation équitable.</h1>
-            <h1>La vidéo de non-responsabilité est uniquement destinée à des fins éducatives. Clause de non-responsabilité relative aux droits d'auteur En vertu de l'article 107 de la loi sur le droit d'auteur de 1976, une « utilisation équitable » est autorisée à des fins telles que la critique, les commentaires, les reportages d'actualité, l'enseignement, l'érudition et la recherche. L'utilisation équitable est une utilisation autorisée par la loi sur le droit d'auteur qui pourrait autrement constituer une violation. Une utilisation à but non lucratif, éducative ou personnelle fait pencher la balance en faveur d'une utilisation équitable.</h1>
+    <div class="vide"></div>
 
-    </div>
-    <div id="overlayText1" class="overlayText">
-        <h1>Digital art</h1> 
-    </div>
-    <div id="overlayText2" class="overlayText">
-        <h1>Code</h1> 
-    </div>
+    <ul id="overlayText" class="overlayText">
+        <li>Web design</li>
+        <li>Photography</li>
+        <li>Code</li>
+        <li>Writing</li>
+        <li>Video & photo composition</li>
+        <li>Music</li>
+        <li>Digital art</li> 
+    </ul>
+    <div class="vide"></div>
+
         <script type="text/javascript">
             const img = document.getElementById('studioImage');
-            const overlayText1 = document.getElementById('overlayText1');
-            const overlayText2 = document.getElementById('overlayText2');
+            const overlayText = document.getElementById('overlayText');
+            const landingpage = document.getElementById('landingpage');
 
             window.addEventListener('scroll', function(){
                 img.style.width = 40 + window.pageYOffset/4 + '%'; 
-                // Affichez le texte lorsque l'image atteint 150% de sa largeur initiale
-            if (parseInt(img.style.width) >= 150) {
-                overlayText1.style.visibility = 'visible';
-                overlayText1.style.position = 'absolute';
-            } else {
-                overlayText1.style.visibility = 'hidden';
-            }
+                if (parseInt(img.style.width) >= 150) {
+                    landingpage.style.backgroundImage = 'url(../img/studio display.png)';
+                    overlayText.style.display = 'flex';
+                } else {
+                    overlayText.style.display = 'none';
+                }
 
-            const threshold = window.innerHeight * 0.8; // Ajustez ce seuil selon vos besoins
-            if (imageTopPosition < threshold) {
-                overlayText2.style.visibility = 'visible';
-                overlayText2.style.position = 'absolute';
-            } else {
-                overlayText2.style.visibility = 'hidden';
-            }
-           
-        });
+                if (parseInt(img.style.width) >= 1170) {
+                    landingpage.style.background = 'none';
+                    img.style.width = 40 - + window.pageYOffset/4 + '%'; 
+                }
+            });
 </script>
 
 </body>
